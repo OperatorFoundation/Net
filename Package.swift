a2db852ee35ec65dded8d46071e350c1d78eed7c
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-09-27 14:59:46 -0500

    Updated macOS version to 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Net",
     platforms: [
-        .macOS(.v10_15), .iOS(.v15)
+        .macOS(.v12),
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
