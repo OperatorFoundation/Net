aabe0ee90aa57475a4d8874789a034aaff3fefa5
bragelbytes <joebragel@gmail.com>
2023-12-14 17:36:27 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -48,7 +48,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/NetworkLinux.git", branch: "main")
+        .package(url: "https://github.com/OperatorFoundation/NetworkLinux", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
